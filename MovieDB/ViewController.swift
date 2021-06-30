aeaad933aaa13c24d7ac8f02faad32bf117de185
Nícolas Almeida <nicogodoyalmeida@outlook.com>
2021-06-30 20:17:35 -0300

    first commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,15 @@
+//chave api (v3 auth): 6c7bcd7c8d501b62b66b43bf32d8c09d
+//Token de leitura da api (V4 auth): eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzdiY2Q3YzhkNTAxYjYyYjY2YjQzYmYzMmQ4YzA5ZCIsInN1YiI6IjYwZGNlZTExYjY4NmI5MDAyZDViNjM0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vfc7QVcJDdylzot9EWZG9UV2iRuCs1Sq_rZlH326GPg
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
